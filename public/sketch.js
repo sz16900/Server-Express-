f91371a5cfdab4d82af913735574d9f6a63c41a8
Seth <sz16900@my.bristol.ac.uk>
2017-09-27 22:03:35 +0100

    Extended server with multiple json files. Score and sentiment analysis

--- public/sketch.js
+++ public/sketch.js
@@ -1,16 +1,37 @@
 function setup() {
 	createCanvas(400, 400);
-	drawData();
+	// drawData();
 	console.log("running");
 
 	var button = select('#submit');
-	button.mousePressed(submitWord);
+	button.mousePressed(submitWord);	
+	var buttonA = select('#analyze');
+	buttonA.mousePressed(analyzeThis);
 }
 
-function drawData() {
-	// p5 seems to be querying from the website to the server. Both seem to be runnning simultaneously
-	loadJSON("all", gotData);
+// function drawData() {
+// 	// p5 seems to be querying from the website to the server. Both seem to be runnning simultaneously
+// 	loadJSON("all", gotData);
+// }
+
+function analyzeThis() {
+	var txt = select('#textinput').value();
+	var data = {
+		text: txt
+	}
+	// This is a p5 function to make POST Convinient
+	// https://p5js.org/reference/#/p5/httpPost
+	httpPost('analyze/', data, 'json', dataPosted, postErr);
 }
+
+function dataPosted(result) {
+	console.log(result);
+}
+
+function postErr(err) {
+	console.log(err);
+}
+
 
 function submitWord() {
 	var word = select('#word').value();
@@ -22,22 +43,21 @@ function submitWord() {
 
 	function finished(data) {
 		console.log(data);
-		drawData();
+		// drawData();
 	}
 }
 
-
-function gotData(data) {
-	background(51);
-	console.log(data);
-	var keys = Object.keys(data);
-	for (i = 0; i < keys.length; i++) {
-		var word = keys[i];
-		var score = data[word];
-		var x = random(width);
-		var y = random(height);
-		fill(255);
-		textSize(16);
-		text(word, x, y);
-	}
-}
+// function gotData(data) {
+// 	background(51);
+// 	console.log(data);
+// 	var keys = Object.keys(data);
+// 	for (i = 0; i < keys.length; i++) {
+// 		var word = keys[i];
+// 		var score = data[word];
+// 		var x = random(width);
+// 		var y = random(height);
+// 		fill(255);
+// 		textSize(16);
+// 		text(word, x, y);
+// 	}
+// }
--- server.js
+++ server.js
@@ -1,18 +1,34 @@
+// TO DO:
+// Take this idea further by allowing training sets.
+// Animations.
+// Interactions how to crowdsource a full word list. Show results in word list with animation.
+// Use CORS to let other people use this API.
+
+//Borrowed seriment from:
+// http://www2.imm.dtu.dk/pubdb/views/publication_details.php?id=6010
+// Using Body-Parser from npm
+
 // Import the file system packages
 var fs = require('fs');
 // Import express
 var express = require('express');
+//Import body-parser
+var bodyParser = require('body-parser');
 
 // raw data
 // Use a synchronus or "block" to wait for this to finish to continue. This is helpful
 // at the start of the server because we want all of the file to load before continuing
-var data = fs.readFileSync('words.json');
+var data = fs.readFileSync('additional.json');
+var afinndata = fs.readFileSync('AFINN-111.json');
 
 // parse it to json
-var words = JSON.parse(data);
+var additional = JSON.parse(data);
+// console.log(data);
+var afinn = JSON.parse(afinndata);
 
 console.log("Hello World");
 
+// An express application which is listening on this port
 var app = express();
 
 var server = app.listen(3000, listening);
@@ -21,7 +37,55 @@ function listening() {
 	console.log("Listening...")
 }
 
+// Uses static hosting on the public folder
 app.use(express.static('public'));
+
+//Parse Everythn as JSON
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.post('/analyze', analyzeThis);
+
+function analyzeThis(request, response) {
+	var totalScore = 0;
+	var wordList = [];
+	//Get the request. Get the body. Get the text.
+	var txt = request.body.text;
+	// Split only words by anything not a letter or number batter matching. Not A-Z 0-9
+	var words = txt.split(/\W+/);
+	for (var i = 0; i < words.length; i++) {
+		var word = words[i];
+		var score = 0;
+		var found = false;
+		if (additional.hasOwnProperty(word)) {
+			score = Number(additional[word]);
+			found = true;
+		}
+		else if (afinn.hasOwnProperty(word)) {
+			score = Number(afinn[word]);
+			found = true;
+		}
+		if (found) {
+			wordList.push({
+				word: word,
+				score: score
+			})
+		}
+		totalScore += score;
+	}
+
+	var comp = totalScore / words.length;
+
+	var reply = {
+		score: totalScore,
+		comparative: comp,
+		words: wordList
+	}
+	response.send(reply);
+}
 
 
 // If a user (or web browser) uses \flower, the the sendFlower function gets executed
@@ -46,11 +110,11 @@ function addWord(request, response) {
 	} 
 	else {
 		// word is the key score is the value
-		words[word] = score;
+		additional[word] = score;
 		// Turn it into a string 
-		var data = JSON.stringify(words, null, 2);
+		var data = JSON.stringify(additional, null, 2);
 		// write too file ( all of it :/ )
-		fs.writeFile('words.json', data, finished);
+		var data = fs.readFileSync('additional.json');
 
 		function finished(err) {
 			reply = {
@@ -68,7 +132,11 @@ app.get('/all', sendAll);
 
 // Express will automatically format your javascript object that you send into the world as json
 function sendAll(request, response) {
-	response.send(words); 
+	var data = {
+		additional: additional,
+		afinn: afinn
+	}
+	response.send(data); 
 }
 
 app.get('/search/:word/', searchWord);
