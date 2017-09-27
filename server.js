02940ac1a765ec8a5922cf474b0e5fd0e91930ad
Seth <sz16900@my.bristol.ac.uk>
2017-09-27 12:05:48 +0100

    First push

---
+++ server.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+var express = require('express');
+
+var app = express();
+
+var server = app.listen(3000, listening);
+
+function listening() {
+	console.log("Listening...")
+}
+
+app.use(express.static('public'));
