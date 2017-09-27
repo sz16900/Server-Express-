7206262788b9b7f6dc0b8accce5b97d7da874a91
Seth <sz16900@my.bristol.ac.uk>
2017-09-27 12:28:56 +0100

    Adding some logic

--- server.js
+++ server.js
@@ -11,3 +11,24 @@ function listening() {
 }
 
 app.use(express.static('public'));
+
+
+// If a user (or web browser) uses \flower, the the sendFlower function gets executed
+// That is the callback
+// Tghe first slash is a rounte, the after colon is a parameter that the user enters
+
+app.get('/search/:flower/:num', sendFlower); 
+
+function sendFlower(request, response){
+
+	var data = request.params;
+	var num = data.num;
+	var reply = "";
+	for (var i = 0; i < num; i++){
+		reply += ("I love " + data.flower + " too!");
+	}
+	response.send(reply);	
+}
+
+
+
